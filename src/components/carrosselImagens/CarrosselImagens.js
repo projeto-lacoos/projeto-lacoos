09f0c0c044ddfc2f02cdb92503c4a58d1e8c4ca2
Filipe Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2023-11-27 17:16:00 -0300

    atualização

--- src/components/carrosselImagens/CarrosselImagens.js
+++ src/components/carrosselImagens/CarrosselImagens.js
@@ -1,12 +1,12 @@
 import './CarrosselImagensModule.css'
 
 import {motion} from 'framer-motion'
+import { useRef } from 'react'
 
 import convite1 from '../../img/carrosselImagens/convite1.jpg'
 import convite2 from '../../img/carrosselImagens/convite2.jpg'
 import convite3 from '../../img/carrosselImagens/convite1.jpg'
 import convite4 from '../../img/carrosselImagens/convite2.jpg'
-import { useRef } from 'react'
 
 const imagens = [convite1, convite2, convite3, convite4]
 
