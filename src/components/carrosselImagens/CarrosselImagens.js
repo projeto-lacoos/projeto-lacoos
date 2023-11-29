a34152852026701ffe200e2b49062cf41c8704b2
Filipe Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2023-11-28 21:24:59 -0300

    att

--- src/components/carrosselImagens/CarrosselImagens.js
+++ src/components/carrosselImagens/CarrosselImagens.js
@@ -1,6 +1,7 @@
 import './CarrosselImagensModule.css'
 
 import {motion} from 'framer-motion'
+import Carousse
 import { useEffect, useRef, useState } from 'react'
 
 import convite1 from '../../img/carrosselImagens/convite1.jpg'
@@ -13,12 +14,6 @@ import NotificacaoLogar from '../../service/notificacaoLogar/NotificacaoLogar'
 const imagens = [convite1, convite2, convite3, convite4, convite5]
 
 export default function CarrosselImagens(){
-    const carroussel = useRef()
-    const [width, setWidth] = useState(0)
-
-    useEffect(() => {
-        setWidth(carroussel.current?.scrollWidth - carroussel.current?.offsetWidth)
-    }, [])
 
     const [notificacao, setNotificacao] = useState(false)
 
@@ -34,17 +29,6 @@ export default function CarrosselImagens(){
 
     return(
         <section className='container-carrossel'>
-            {notificacao === true ? (<NotificacaoLogar />) : ("")}
-            <h2 className='titulo-carrossel'>Modelos de Convites</h2>
-            <motion.div ref={carroussel} className='carousel' whileTap={{ cursor: "grabbing" }}>
-                <motion.div className='inner' drag="x" dragConstraints={{ right: 0, left: -width }}>
-                    {imagens.map((imagem, id) => (
-                        <motion.div className='item' key={id} onClick={HandleNotificacao}>
-                            <img className='imagemItem' src={imagem} alt='Texto alternativo' />
-                        </motion.div>
-                    ))}
-                </motion.div>
-            </motion.div>
         </section>
     )
 }
