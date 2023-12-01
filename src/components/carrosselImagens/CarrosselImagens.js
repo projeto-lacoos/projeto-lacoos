6122f770174531d6ab5ac4bd201863513521dbe2
Nathan V Garrett <ngarrett768@gmail.com>
2023-11-30 22:52:05 -0300

    Carrosel imagem

--- src/components/carrosselImagens/CarrosselImagens.js
+++ src/components/carrosselImagens/CarrosselImagens.js
@@ -1,6 +1,6 @@
 import './CarrosselImagensModule.css'
 
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
 
 import convite1 from '../../img/carrosselImagens/convite1.jpg'
@@ -10,9 +10,9 @@ import convite4 from '../../img/carrosselImagens/convite2.jpg'
 import convite5 from '../../img/carrosselImagens/convite1.jpg'
 import NotificacaoLogar from '../../service/notificacaoLogar/NotificacaoLogar'
 
-const imagens = [convite1, convite2, convite3, convite4, convite5]
+const imagens = [convite1, convite2, convite3, convite4, convite5];
 
-export default function CarrosselImagens(){
+export default function CarrosselImagens() {
     const carroussel = useRef()
     const [width, setWidth] = useState(0)
 
@@ -23,16 +23,16 @@ export default function CarrosselImagens(){
     const [notificacao, setNotificacao] = useState(false)
 
     const HandleNotificacao = () => {
-      if (notificacao === false){
-        setNotificacao(true)
-        console.log("teste",notificacao);
-    } else {
-        setNotificacao(false)
-        console.log("teste",notificacao);
-      }
+        if (notificacao === false) {
+            setNotificacao(true)
+            console.log("teste", notificacao);
+        } else {
+            setNotificacao(false)
+            console.log("teste", notificacao);
+        }
     }
 
-    return(
+    return (
         <section className='container-carrossel'>
             {notificacao === true ? (<NotificacaoLogar />) : ("")}
             <h2 className='titulo-carrossel'>Modelos de Convites</h2>
