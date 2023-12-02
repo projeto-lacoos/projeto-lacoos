42e6fa2f2897ff0ca1ff67477f19a0d426681e1c
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2023-12-02 08:50:06 -0300

    Projeto demoday 02/12/2023
    - Responsividade em todas as telas da página recuperação da senha

--- src/components/pages/recuperacaoSenha/RecuperacaoSenha.js
+++ src/components/pages/recuperacaoSenha/RecuperacaoSenha.js
@@ -1,42 +1,36 @@
 import "./RecuperacaoSenhaModule.css";
 
-import LogoReduzido from "../../../img/recuperacaoSenha/logoReduzido.png"
+import LogoReduzido from "../../../img/recuperacaoSenha/logoReduzido.png";
 import Input from "../../input/Input";
 import { Link } from "react-router-dom";
 
 export default function RecuperacaoSenha() {
   return (
     <div className="container-recuperacao-senha">
-      <div className="white">
-        <img
-          src={LogoReduzido}
-          className="logo-reduzido"
-          alt="Logo Entrelaçoos"
-        />
+      <div className="lado-e">
+        <img src={LogoReduzido} className="logo-reduzido" alt="Logo Entrelaçoos"/>
       </div>
-      <div className="image">
+      <div className="lado-d">
         <div className="box">
           <div className="textos">
-            <h1>Recuperação de senha</h1>
-            <p>
-              Para recuperarmos sua senha, informe seu endereço de e-mail para
-              que possamos enviar um link de redefinição de senha.
-            </p>
+            <h1 className="h1-textos">Recuperação de senha</h1>
+            <p className="p-textos">Para recuperarmos sua senha, informe seu endereço de e-mail para que possamos enviar um link de redefinição de senha.</p>
           </div>
-          <div className="valores">
-            <label htmlFor={"email_usuario"}>E-mail</label>
-            <Input type={"text"} placeholder={"Exemplo.sac@xxxx.com"} name={"email_usuario"}/>
-          </div>
-          <div className="button-recuperacao-senha">
-            <button>Enviar</button>
-          </div>
-          <Link
-            style={{ textDecoration: "none" }}
-            to={"/login"}
-            className="voltar-recuperacao-senha"
-          >
-            <p>Voltar</p>
-          </Link>
+          <form className="form-recup-senh" action="">
+            <div className="valores">
+              <label className="valor-label" htmlFor={"email_usuario"}>E-mail</label>
+              <Input
+                type={"text"}
+                placeholder={"Exemplo.sac@xxxx.com"}
+                name={"email_usuario"}
+                className={"input-valor"}
+              />
+            </div>
+            <div className="button-recuperacao-senha">
+              <button type="submit" className="btn-recup-senha">Enviar</button>
+            </div>
+          </form>
+          <Link style={{ textDecoration: "none" }} to={"/login"} className="voltar-recuperacao-senha">Voltar</Link>
         </div>
       </div>
     </div>
