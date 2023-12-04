71e7630f68d76460954edd805c8b2b570d809aba
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2023-12-04 17:26:53 -0300

    Projeto demoday 04/12/2023
    - Estilizando botão desconectar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -72,7 +72,7 @@ export default function Navbar() {
                     </Link>
                   </li>
                   <li>
-                    <Link>
+                    <Link className="item">
                       <button
                         onClick={() => {
                           logoutUser();
