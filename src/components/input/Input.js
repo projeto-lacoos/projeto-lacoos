15ea8c3d3492370afc372c72b01cc0a46df3a12b
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2023-12-04 19:45:48 -0300

    Projeto demoday 04/12/2023
    - Responsividade em todas as telas página pagamentos

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,5 +1,5 @@
 export default function Input({className, type, placeholder, name, id, onchange}){
     return (
-        <input className={className} type={type} placeholder={placeholder} name={name} id={id} onChange={onchange} />
+        <input className={className} type={type} required placeholder={placeholder} name={name} id={id} onChange={onchange} />
     )
 }
