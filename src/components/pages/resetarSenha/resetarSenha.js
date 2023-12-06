d68561983e2b8dc09ffcf8f1060ea9806d548b0b
Nathan V Garrett <ngarrett768@gmail.com>
2023-12-06 00:39:46 -0300

    @nathangarret backend integração

--- src/components/pages/resetarSenha/resetarSenha.js
+++ src/components/pages/resetarSenha/resetarSenha.js
@@ -2,17 +2,66 @@ import "./resetarSenha.css";
 
 import Input from "../../input/Input";
 
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
 export default function ResetarSenha() {
+
+  const params = useParams();
+
+  const navigate = useNavigate();
+
+  const [senha, setSenha] = useState('');
+  const [confirmaSenha, setConfirmaSenha] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (senha === confirmaSenha) {
+      try {
+        let token;
+        const response = await fetch(`http://localhost:8080/v1/user/reset-password/${params.token}`, // ${} | 87b6246c-35e7-4bc8-9e07-0334e79ca2f7
+          {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              password: senha,
+              confirmPassword: confirmaSenha
+            })
+          });
+        console.log(`${token}`);
+
+        const data = await response.json();
+        console.log(data);
+
+        if (typeof window !== undefined && response.ok) {
+          navigate("/login");
+        }
+      } catch (error) {
+        console.log(error);
+        alert('Algo deu errado! Mas enviou o email!');
+      }
+    } else {
+      console.log("Informe as senhas iguais.");
+    }
+
+  }
+
   return (
     <div className="container-resetar-senha">
       <div className="img-resetar-senha">
-        <Input placeholder={"Senha"}>Senha</Input>
+        <Input placeholder={"Senha"} onchange={(e) => {
+          setSenha(e.target.value)
+        }}>Senha</Input>
         <br />
         <br />
-        <Input placeholder={"Confirmar senha"}>Confirmar senha</Input>
+        <Input placeholder={"Confirmar senha"} onchange={(e) => {
+          setConfirmaSenha(e.target.value)
+        }}>Confirmar senha</Input>
         <br />
         <br />
-        <button>Salvar</button>
+        <button onClick={onSubmit}>Salvar</button>
       </div>
     </div>
   );
