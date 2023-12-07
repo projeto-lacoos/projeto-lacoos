5085d3993484e6583c6f02341d50046ba923c094
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2023-12-07 16:26:52 -0300

    Projeto demoday 07/12/2023
    - Deixando a página de login acessível

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 import Input from "../../input/Input";
 import { ApplicationContext } from "../../context/ApplicationProvider";
 import { jwtDecode } from 'jwt-decode'
-import Logo from "../../../img/login/Logo2.png";
 import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "../../context/ThemeContext";
+
+import LogoReduzida from '../../../img/login/logo-reduzida.svg'
+import DPTT from "../../../img/login/oo.svg";
+import M from "../../../img/login/oo-2.svg";
+import PD from "../../../img/login/oo-1.svg";
+import A from "../../../img/login/oo-3.svg";
 
 export default function Login() {
   const { theme } = useContext(ThemeContext);
@@ -25,6 +30,22 @@ export default function Login() {
         return "default";
     }
   };
+
+  const getThemeLogo = (theme) => {
+    switch (theme) {
+      case "DPTT":
+        return DPTT;
+      case "M":
+        return M;
+      case "PD":
+        return PD;
+      case "A":
+        return A;
+      default:
+        return LogoReduzida;
+    }
+  };
+
   const { setUser, setLocalUser, setAuth } = useContext(ApplicationContext);
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
@@ -70,32 +91,32 @@ export default function Login() {
   }
 
   return (
-    <div className="header_logo">
+    <div className={`header_logo ${getThemeClass(theme)}`}>
       <ul className="nav_logo">
-        <li>BEM VINDO</li>
+        <li className={`${getThemeClass(theme)}`}>BEM VINDO</li>
         <li>
           <Link to={"/"}>
-            <img src={Logo} alt="Logo imagem" />
+            <img src={getThemeLogo(theme)} alt="Logo da Laçoos, onde é uma imagem de duas alianças se entrelaçando" />
           </Link>
         </li>
       </ul>
       <section className="container-login">
         <div className="form-login">
-          <h2 className="title_form">LOGIN</h2>
-          <label className="label_login">
+          <h2 className={`title_form ${getThemeClass(theme)}`}>LOGIN</h2>
+          <label className={`label_login ${getThemeClass(theme)}`}>
             E-mail
-            <Input className={"input-login"} type={"email"} placeholder={"exemplo.sac@xxxx.com"} name={"email_usuario"} onchange={(e) => { setEmail(e.target.value) }} />
+            <Input className={`input-login ${getThemeClass(theme)}`} type={"email"} placeholder={"exemplo.sac@xxxx.com"} name={"email_usuario"} onchange={(e) => { setEmail(e.target.value) }} />
           </label>
-          <label className="label_login">
+          <label className={`label_login ${getThemeClass(theme)}`}>
             Senha
-            <Input className={"input-login"} type={"password"} placeholder={"Digite sua senha"} name={"senha_usuario"} onchange={(e) => { setSenha(e.target.value) }} />
+            <Input className={`input-login ${getThemeClass(theme)}`} type={"password"} placeholder={"Digite sua senha"} name={"senha_usuario"} onchange={(e) => { setSenha(e.target.value) }} />
           </label>
-          <button className="button_login" onClick={(e) => { onSubmit(e) }}>ENTRAR</button>
-          <div className="container-links-login">
-            <Link to={"/recuperacao-senha"} className="link_login_password">
+          <button className={`button_login ${getThemeClass(theme)}`} onClick={(e) => { onSubmit(e) }}>ENTRAR</button>
+          <div className={`container-links-login`}>
+            <Link to={"/recuperacao-senha"} className={`link_login_password ${getThemeClass(theme)}`}>
               Esqueceu a senha?
             </Link>
-            <Link to="/nova-conta" className="link_creat_cont">
+            <Link to="/nova-conta" className={`link_creat_cont ${getThemeClass(theme)}`}>
               Não tem conta ainda? Crie agora
             </Link>
           </div>
